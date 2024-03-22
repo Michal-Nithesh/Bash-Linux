15a8577665fea569e9829e680649e56b334ae09f
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-03-23 01:34:05 +0530

    Update Himom.sh

--- Himom.sh
+++ Himom.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+
+# Basic
+# which SHELL
+# echo "Hi Mom" Runtime
+# nano Himom.sh Create a file in the nano notepad
+# bash Himom.sh Executing the file
+# ./Himom Another method in this method we need to change the mod 
+# ls -l
+# chmod +x Himom.sh
+# Now you can execute ./Himom.sh
+# date
+# whoami
+
 echo "Hi Mom"
 
 sleep 3
