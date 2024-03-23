923a4df25deb901a8db3efe13d20e5ec1d944817
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-03-23 12:49:52 +0530

    Update GetRichQuick.sh

--- Day-03/GetRichQuick.sh
+++ Day-03/GetRichQuick.sh
@@ -17,3 +17,18 @@ read name
 echo "How old are you ?"
 
 read age
+
+# New Concept
+echo $RANDOM # it is a built in variable in Linux 
+# Range of RANDOM is (0 - 32767)
+
+echo "$SHELL, $PED, $USER, $HOSTNAME"
+
+# Runtime (echo twitter="Elon Mask")
+echo $twitter
+
+# nano GetRichQuick.sh
+echo "$twitter"
+
+# compile 
+export twitter
