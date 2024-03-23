d56ff545a7d0c8587aaa7b1164d76987707c9bfd
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-03-23 12:49:06 +0530

    Create Math.sh

---
+++ Day-04/Math.sh
@@ -0,0 +1,14 @@
+# New Concept
+echo $RANDOM # it is a built in variable in Linux 
+# Range of RANDOM is (0 - 32767)
+
+echo "$SHELL, $PED, $USER, $HOSTNAME"
+
+# Runtime (echo twitter="Elon Mask")
+echo $twitter
+
+# nano GetRichQuick.sh
+echo "$twitter"
+
+# compile 
+export twitter
