02e00a9ff14e52c55ab53c1a226d528a2bb33170
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-03-23 12:41:54 +0530

    Create GetRichQuick.sh

---
+++ Day-03/GetRichQuick.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+name=$1 # name is a variable
+
+age=$2 #age is a variable
+
+echo "What is your Name : $name"
+
+echo "What is you Age : $age"
+
+echo "Hello, $name. You are $age years old"
+
+echo "What is your Name ?"
+
+read name
+
+echo "How old are you ?"
+
+read age
